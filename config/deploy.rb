55c903ecee0601ed8aed104dcdc037677a21c10f
yoni <yoniros@gmail.com>
2016-12-17 17:21:18 -0800

    Added deploy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid only for current version of Capistrano
 lock "3.7.1"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "contactbook"
+set :repo_url, "git@github.com:yoniros/contactbook.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
